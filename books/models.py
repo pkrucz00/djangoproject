c78aa5dca942dea2ddca04d9ae0e7e3a1197a564
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2020-05-25 11:58:49 +0200

    zajecia 25.05

--- books/models.py
+++ books/models.py
@@ -8,7 +8,21 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     short_description = models.TextField()
     published_at = models.DateTimeField()
-    author = models.CharField(max_length=128, null=True)
+
+    author = models.ManyToManyField(to="books.Author",
+                                    verbose_name="autorzy",
+                                    related_name="books")
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "książka"
+        verbose_name_plural = "książki"
 
     def __str__(self):
-        return self.title
+        return "Książka: " + self.title
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    about = models.TextField(verbose_name="o autorze", blank=True)
+    photo = models.ImageField(verbose_name="zdjęcie", blank=True)
